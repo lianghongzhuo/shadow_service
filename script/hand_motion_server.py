0d661f5ed356e437fc43ff477c512779ec8dde91
Hongzhuo Liang <lianghongzhuo@126.com>
2021-04-30 12:58:19 +0200

    simplify code

--- script/hand_motion_server.py
+++ script/hand_motion_server.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import rospy
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 from shadow_service.srv import ShadowCommanderSrv, ShadowCommanderSrvResponse
-from get_joint_limits import get_joint_limits
+from get_joint_limits import get_joint_limits, JOINT_NAMES
 import numpy as np
 
 
@@ -40,40 +40,12 @@ class ShadowCommanderServer:
         goal = joints_msg.joint_positions.data
         goal = self.clip_hand_pos(goal)
         # wrist
-        hand_joint_positions = {
-                                # wrist
-                                self.name_prefix + "_WRJ2": goal[0],
-                                self.name_prefix + "_WRJ1": goal[1],
-                                # first finger
-                                self.name_prefix + "_FFJ4": goal[2],
-                                self.name_prefix + "_FFJ3": goal[3],
-                                self.name_prefix + "_FFJ2": goal[4],
-                                self.name_prefix + "_FFJ1": goal[5],
-                                # middle finger
-                                self.name_prefix + "_MFJ4": goal[6],
-                                self.name_prefix + "_MFJ3": goal[7],
-                                self.name_prefix + "_MFJ2": goal[8],
-                                self.name_prefix + "_MFJ1": goal[9],
-                                # ring finger
-                                self.name_prefix + "_RFJ4": goal[10],
-                                self.name_prefix + "_RFJ3": goal[11],
-                                self.name_prefix + "_RFJ2": goal[12],
-                                self.name_prefix + "_RFJ1": goal[13],
-                                # little finger
-                                self.name_prefix + "_LFJ5": goal[14],
-                                self.name_prefix + "_LFJ4": goal[15],
-                                self.name_prefix + "_LFJ3": goal[16],
-                                self.name_prefix + "_LFJ2": goal[17],
-                                self.name_prefix + "_LFJ1": goal[18],
-                                # thumb
-                                self.name_prefix + "_THJ5": goal[19],
-                                self.name_prefix + "_THJ4": goal[20],
-                                self.name_prefix + "_THJ3": goal[21],
-                                self.name_prefix + "_THJ2": goal[22],
-                                self.name_prefix + "_THJ1": goal[23],
-                                }
+        hand_joint_positions = {}
+        for i, joint in enumerate(JOINT_NAMES):
+            joint = joint.format(self.name_prefix)
+            hand_joint_positions[joint] = goal[i]
 
-        rospy.loginfo("getting shadow hand command {}".format(hand_joint_positions))
+        rospy.loginfo("getting shadow hand command {}".format(goal))
         if self.safe_mode:
             self.hand_commander.move_to_joint_value_target(hand_joint_positions, angle_degrees=False)
         else:
